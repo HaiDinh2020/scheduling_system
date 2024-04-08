3e6b5032ab5d0a318f42d06f50eaeaa151d779c3
HaiDinh2020 <haidinh6008@gmail.com>
2024-04-08 09:36:43 +0700

    create api login, register

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express  from "express";
+require("dotenv").config()
+import cors from 'cors';
+import initRoutes from './src/routes'
+import connectDatabase from "./src/config/connectDatabase";
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+initRoutes(app)
+connectDatabase()
+
+
+const port = process.env.PORT || 8888;
+const listen = app.listen(port, ()=> {
+    console.log(`server is run on port ${listen.address().port}`)
+})
---
+++ server/src/controllers/auth.js
@@ -0,0 +1,46 @@
+import { loginService, registerService } from "../services/auth"
+
+export const login = async (req, res) => {
+    const { email, password} = req.body
+    try {
+        if( !email || !password) {
+            return res.status(400).json({
+                err:1,
+                msg:"Missing input!"
+            })
+        }
+        
+        const response = await loginService(req.body)
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).json({
+            err: -1,
+            msg: 'Fail at auth controller' + error
+        })
+    }
+
+}
+
+export const register = async (req, res) => {
+    const {name, email, password} = req.body
+
+    try {
+        
+        if(!name || !email || !password) {
+            return res.status(400).json({
+                err:1,
+                msg:"Missing input!"
+            })
+        }
+
+        const response = await registerService(req.body)
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).json({
+            err: -1,
+            msg: 'Fail at auth controller' + error
+        })
+    }
+
+}
+
