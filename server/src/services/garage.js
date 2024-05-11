ee07a25e435cb289eb17d7d04803b6de08aa8d76
HaiDinh2020 <haidinh6008@gmail.com>
2024-05-12 03:00:45 +0700

    change to sequelize

--- server/src/services/garage.js
+++ server/src/services/garage.js
@@ -3,69 +3,74 @@ require('dotenv').config()
 
 import pool from '../config/connectDB'
 
-export const getGarageInforServices = (userId) => new Promise(async(resolve, reject) => {
-    try {
-        const sql = `select garages.id, garage_name, address, introduce, website, business_hours, services, star, images, owner_id from garages, users  where garages.owner_id = users.id and users.id = '${userId}'`;
-        pool.execute(sql, (err, result) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve({
-                err: 0,
-                msg: "success to get garage infor",
-                response: result[0]
-              });
-            }
-          });
-    } catch (error) {
-        reject(error)
-    }
-})
-
-export const getGarageInforServicesV2 = (garageId) => new Promise(async(resolve, reject) => {
+export const getGarageInforServices = (userId) => new Promise(async (resolve, reject) => {
   try {
-      const sql = `select id, garage_name, address, introduce, website, business_hours, services, star, images, owner_id from garages where id = '${garageId}'`;
-      pool.execute(sql, (err, result) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve({
-              err: 0,
-              msg: "success to get garage infor",
-              response: result[0]
-            });
-          }
-        });
-  } catch (error) {
-      reject(error)
-  }
-})
-
-export const getAllGarageServices = () => new Promise(async(resolve, reject) => {
-  try {
-    const sql = `select * from garages`
+    const sql = `select garages.id, garage_name, address, introduce, website, business_hours, services, star, images, owner_id from garages, users  where garages.owner_id = users.id and users.id = '${userId}'`;
     pool.execute(sql, (err, result) => {
-      if(err) {
-        reject(err)
+      if (err) {
+        reject(err);
       } else {
         resolve({
           err: 0,
-          msg: "success to get all garage",
-          response: result
-        })
+          msg: "success to get garage infor",
+          response: result[0]
+        });
       }
+    });
+  } catch (error) {
+    reject(error)
+  }
+})
+
+export const getGarageInforServicesV2 = (garageId) => new Promise(async (resolve, reject) => {
+  try {
+    const sql = `select id, garage_name, address, introduce, website, business_hours, services, star, images, owner_id from garages where id = '${garageId}'`;
+    pool.execute(sql, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({
+          err: 0,
+          msg: "success to get garage infor",
+          response: result[0]
+        });
+      }
+    });
+  } catch (error) {
+    reject(error)
+  }
+})
+
+// done
+export const getAllGarageServices = () => new Promise(async (resolve, reject) => {
+  try {
+    const response = await db.Garage.findAll({
+      raw: true,
+      nest: true,
+      include: [
+        { model: db.User, as: 'user', attributes: ['name', 'phone', 'avatar'] },
+      ]
+    })
+
+    if (!response) {
+      reject(err)
+    }
+    resolve({
+      err: 0,
+      msg: "success to get all garage",
+      response: response
     })
   } catch (error) {
     reject(error)
   }
 })
 
-export const updateGarageInforServices = (garageId, garage_name, address, introduce, website, business_hours, services, star, images) => new Promise(async(resolve, reject) => {
+export const updateGarageInforServices = (garageId, garage_name, address, introduce, website, business_hours, services, star, images) => new Promise(async (resolve, reject) => {
   try {
     const sql = `UPDATE garages SET garage_name = '${garage_name}', address = '${address}', introduce = '${introduce}', website = '${website}', business_hours = '${business_hours}', services = '${services}', star = '${star}', images = '${images}'
                 WHERE id = '${garageId}'`
     pool.execute(sql, (err, result) => {
-      if(err) {
+      if (err) {
         reject(err);
       } else {
         console.log(result)
